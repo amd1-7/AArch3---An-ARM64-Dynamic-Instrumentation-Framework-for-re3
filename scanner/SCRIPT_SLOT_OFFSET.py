64ae138009db5e030e1ea0480529f5aae31cc02b
amd1.7 <amedbakayoko75@gmail.com>
2026-09-22 22:58:57 +0200

    feat(SCRIPT_SLOT_OFFSET.py): This file read and extract the offset of slot' variable

---
+++ scanner/SCRIPT_SLOT_OFFSET.py
@@ -0,0 +1,36 @@
+from LLDB_INIT import lldb,process,logger
+import json
+
+thread:lldb.SBThread = process.GetSelectedThread()
+frame:lldb.SBFrame = thread.GetSelectedFrame()
+
+player:lldb.SBValue = frame.EvaluateExpression("FindPlayerPed()")
+slots:lldb.SBValue = player.GetChildMemberWithName("m_weapons")
+
+book = {}
+playerPtr:lldb.SBValue = player.AddressOf().GetValueAsUnsigned()
+for i in range(0,13):
+    slot:lldb.SBValue = slots.GetChildAtIndex(i)
+    slotAddr:lldb.SBValue = slot.AddressOf().GetValueAsUnsigned()
+
+    weapon_type:lldb.SBValue = slot.GetChildMemberWithName("m_eWeaponType").AddressOf().GetValueAsUnsigned() - slotAddr
+    weapon_state:lldb.SBValue = slot.GetChildMemberWithName("m_eWeaponState").AddressOf().GetValueAsUnsigned() - slotAddr
+    ammo_clip:lldb.SBValue = slot.GetChildMemberWithName("m_nAmmoInClip").AddressOf().GetValueAsUnsigned() - slotAddr
+    ammo_total:lldb.SBValue = slot.GetChildMemberWithName("m_nAmmoTotal").AddressOf().GetValueAsUnsigned() - slotAddr
+    timer:lldb.SBValue = slot.GetChildMemberWithName("m_nTimer").AddressOf().GetValueAsUnsigned() - slotAddr
+    add_rot_offset:lldb.SBValue = slot.GetChildMemberWithName("m_bAddRotOffset").AddressOf().GetValueAsUnsigned() - slotAddr
+
+    book[f"slot {i}"] = {
+        "slot" : slotAddr - playerPtr,
+        "weapon_type" :weapon_type, 
+        "weapon_state" : weapon_state,
+        "ammo_clip" : ammo_clip,
+        "ammo_total" : ammo_total,
+        "timer" : timer,
+        "add_rot_offset" : add_rot_offset,
+        }
+
+    offsetLastSlot = slot.AddressOf().GetValueAsUnsigned()
+
+with open('outputScript/offset_of_slots.json','w') as f:
+    json.dump(book,f,indent=4)
