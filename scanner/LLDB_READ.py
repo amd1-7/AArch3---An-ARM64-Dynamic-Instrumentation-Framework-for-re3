7781eb4faaa458d57087626d0371872c0bba8458
amd1.7 <amedbakayoko75@gmail.com>
2026-09-25 20:51:58 +0200

    feat(framework/native + framework/GunsGunsGunsModule.py + LLDB_READ.py): -framework/native:This folder will contains .so and pyi files.
    GunsGunsGunsModule.py :This file initialize the module GunsGunsGuns
    -LLDB_READ.py:this update add getting of player's player

--- scanner/LLDB_READ.py
+++ scanner/LLDB_READ.py
@@ -5,5 +5,16 @@ if pid == lldb.LLDB_INVALID_PROCESS_ID:
     logger.error(f"[LLDB_READ.py] error get PID")
     exit()
 
+
 thread:lldb.SBThread = process.GetSelectedThread()
+frame:lldb.SBFrame = thread.GetSelectedFrame()
+
+player:lldb.SBValue = frame.EvaluateExpression("FindPlayerPed()")
+playerPtr:lldb.SBValue = player.GetValueAsUnsigned()
+
+if not playerPtr:
+    logger.error("[LLDB_READ.py] error get playter's pointer")
+    exit()
+
+logger.success("[LLDB_READ.py] success get playter's pointer")
 process.Detach()
